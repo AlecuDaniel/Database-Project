3980ce9c2eb949a427b7b5773fbe91b474de8c49
costi <costi@LAPTOP-EKHLUO7S>
2025-04-22 17:04:00 +0300

    Models

---
+++ Database-Project/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Database_Project.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [StringLength(20)]
+        public string ISBN { get; set; }
+
+        [StringLength(255)]
+        public string Authors { get; set; }
+
+        [StringLength(100)]
+        public string Publisher { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<BookStock> BookStocks { get; set; }
+        public ICollection<BorrowRecord> BorrowRecords { get; set; }
+    }
+}
---
+++ Database-Project/Models/BorrowRecord.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Database_Project.Models
+{
+    public class BorrowRecord
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [Required]
+        [ForeignKey("Book")]
+        public int BookId { get; set; }
+
+        [Required]
+        [ForeignKey("LibraryBranch")]
+        public int BranchId { get; set; }
+
+        [Required]
+        public DateTime BorrowDate { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        public DateTime? ReturnDate { get; set; }
+
+        public bool IsOverdue { get; set; }
+
+        public User User { get; set; }
+        public Book Book { get; set; }
+        public LibraryBranch LibraryBranch { get; set; }
+    }
+}
